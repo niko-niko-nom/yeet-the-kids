b17c3a4b358d76ac3b9a1a9c40ebe995f4f25bb9
Jeroen van de Geest <jeroen@vandegeest.eu>
2024-09-19 15:00:23 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Yeet the kids";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system: 
+    let
+      pkgs = (import nixpkgs) { inherit system; };
+    in {
+      devShell = pkgs.mkShell {
+        name = "Yeet the Kids";
+        buildInputs = with pkgs; [
+          python312
+          python312Packages.django
+        ];
+
+        shellHook = ''
+          echo ""
+          python --version
+          echo "
+          Welcome to the Yeet the Kids shell 🚀!
+          ";
+        '';
+      };
+    }
+  );
+}
