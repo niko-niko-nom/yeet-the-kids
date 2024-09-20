3e2461d97f67b9a708016f929f16ac4e1fd34eb0
niko-niko-nom <nicolebelletje@gmail.com>
2024-09-20 16:19:40 +0200

    footer en header styles verplaatst naar de componenten

--- components/header.js
+++ components/header.js
@@ -5,6 +5,60 @@ class Header extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
+        <style>    
+
+            /* header elements */
+            header {
+                background-color: var(--the);
+                position: fixed;
+                top: 0;
+                width: 100%;
+                height: 60px;
+                display: flex;
+                align-items: center;
+                font-size: auto;
+            }
+
+            nav {
+                width: 100%;
+                height: 100%;
+                display: flex;
+                justify-content: center;
+                padding: 5px;
+                color: var(--light);
+            }
+
+            nav a {
+                height: 100%;
+                text-decoration: none;
+                color: var(--light);
+            }
+
+            nav div {
+                height: 100%;
+                width: auto;
+                padding: 0 25px 0 25px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                font-weight: bold;
+            }
+
+            nav div:hover {
+                box-shadow: inset 0 0 0 4px var(--yeet), 
+                            inset 0 0 0 8px var(--the);
+                background: var(--darker);
+                transition: 0.2s;
+            }
+
+            header img {
+                max-height: 60px;
+                padding: 5px;
+            }
+
+        </style>
+
+
         <header>
             <nav id="nav">
                 <a href="dashboard.html"><div>Home</div></a>
