eaaf4adf3859cf85e2a4a2d524ef788cb089d123
Jeroen van de Geest <jeroen@vandegeest.eu>
2024-09-20 11:45:32 +0200

    Fix the game

--- YTK/settings.py
+++ YTK/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = '/static/'
 STATIC_FILES_DIRS = [
         BASE_DIR / "static"
 ]
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
