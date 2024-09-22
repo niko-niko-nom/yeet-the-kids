fb68c13608096ec6a1e4d8e82e8905a6afe9da79
Jeroen van de Geest <jeroen@vandegeest.eu>
2024-09-22 13:16:17 +0200

    Delete user model from APPYTK

--- APPYTK/models.py
+++ APPYTK/models.py
@@ -1,6 +1,1 @@
 from django.db import models
-
-class User(models.Model):
-    name = models.CharField(max_length=24)
-    password = models.CharField(max_length=100)
-    permissions = models.IntegerField()
