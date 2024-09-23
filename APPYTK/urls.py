c4933acf1133d627f5bb8585c96c0dae095a707b
ThomasHU612 <thomas.middelbos@student.hu.nl>
2024-09-23 19:45:34 +0200

    Kaas 23-9-24 _19:45

--- APPYTK/urls.py
+++ APPYTK/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+<<<<<<< Updated upstream
     path("", views.home, name="home")
+=======
+    path("", views.home, name="home"),
+>>>>>>> Stashed changes
 ]
+  
