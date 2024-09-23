286b86ec047fbecaac3dcb78076b749d8101bc12
Jeroen van de Geest <jeroen@vandegeest.eu>
2024-09-23 09:26:14 +0200

    Add AnnouncementView

--- announcements/views.py
+++ announcements/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 
-# Create your views here.
-
 class AnnouncementListView(View):
+    def get(request):
+
+class AnnouncementView(View):
     def get(request):
         
