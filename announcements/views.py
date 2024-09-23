0f39963893189fc66c8290aa521f0a307f415cfd
Ahomadu <ahmad.akkad@student.hu.nl>
2024-09-23 12:09:49 +0200

    Update views.py

--- announcements/views.py
+++ announcements/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from django.views import View
 from . import models
 from . import forms
@@ -17,5 +17,15 @@ class CreateAnnouncement(View):
     def get(self, request):
         form = forms.Editform()
         return render(request, "edit_announcements.html", {'form':form})
-    
 
+    def post(self, request):
+        form = forms.Editform(request.POST)
+
+        if not form.is_valid():
+            return render(request, "edit_announcements.html", {'form':form})
+        
+        announcement = models.Announcement(title=form.cleaned_data["title"], text=form.cleaned_data["text"])
+        announcement.save()
+
+        return redirect("announcement", id=announcement.id)
+        
