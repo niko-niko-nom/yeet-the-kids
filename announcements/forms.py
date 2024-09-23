7596d4d5b66c09be14108c7f8a72cfaf0c94be7e
Jeroen van de Geest <jeroen@vandegeest.eu>
2024-09-23 11:43:57 +0200

    Fix form

--- announcements/forms.py
+++ announcements/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 class Editform(forms.Form):
     title = forms.CharField(max_length=32)
-    text = forms.Textarea()
+    text = forms.CharField(min_length=2, widget=forms.Textarea)
