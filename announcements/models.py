ec2312166ac61c35eb6f6ccf37ff3307342510bf
Ahomadu <ahmad.akkad@student.hu.nl>
2024-09-23 10:44:18 +0200

    push

--- announcements/models.py
+++ announcements/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Announcements(models.Model)
+class Announcement(models.Model):
     title = models.CharField(max_length=32)
     text = models.TextField()
-
-    
