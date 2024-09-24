795db9fb492f891ad929013d272a3d69ead1e877
Jeroen van de Geest <jeroen@vandegeest.eu>
2024-09-24 14:29:55 +0200

    Calculate vote thing

--- kaasinc/views.py
+++ kaasinc/views.py
@@ -8,6 +8,7 @@ def kaas(request):
     return render(request, 'kaas.html')
 
 def stem_van_de_week(request):
+    form = forms.VoteForm()
     if request.method == "POST":
         form = forms.VoteForm(request.POST)
 
@@ -15,14 +16,21 @@ def stem_van_de_week(request):
             return render(request, 'kaas_vote.html', { 'form': form })
 
         try:
-            vote = models.Vote(ip=request.ip, vote=forms.cleaned_data["vote"])
+            vote = models.Vote(ip=request.META['REMOTE_ADDR'], vote=form.cleaned_data["vote"])
             vote.save()
+            messages.add_message(request, messages.SUCCESS, "Added vote")
         except:
-            messages.add_message(request, messages.SUCCESS, "You already voted")
-            return render(request, 'kaas_vote.html', {'form': form})
-        
-        messages.add_message(request, messages.SUCCESS, "Added vote")
-        return render(request, 'kaas_vote.html', {'form': form})
+            messages.add_message(request, messages.SUCCESS, "Already voted")
 
-    form = forms.VoteForm()
-    return render(request, 'kaas_vote.html', {'form': form})
+    voteModels = list(models.Vote.objects.all())
+    votes = []
+    total = 0;
+
+    for vote in voteModels:
+        while len(votes)-1 < vote.vote:
+            votes.append(0)
+        v = votes[vote.vote]
+        votes[vote.vote] = v+1
+        total += 1
+
+    return render(request, 'kaas_vote.html', {'form': form, 'votes': votes, 'total': total})
