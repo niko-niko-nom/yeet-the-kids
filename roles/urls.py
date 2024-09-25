f6bd8f36c6469484509a93ae351b01488501341c
Ahomadu <ahmad.akkad@student.hu.nl>
2024-09-25 10:14:46 +0200

    kk

--- roles/urls.py
+++ roles/urls.py
@@ -6,5 +6,6 @@ from . import forms
 urlpatterns = [
     path("", views.RoleListView.as_view(), name='roles'),
     path("<int:id>/", views.RolesView.as_view(), name='role'),
-    path("new/", views.CreateRoles.as_view(), name='new role')
+    path("new/", views.CreateRoles.as_view(), name='new role'),
+    path("<int:id>/edit/", views.EditRole.as_view(), name= 'role edit')
 ]
--- roles/views.py
+++ roles/views.py
@@ -13,14 +13,40 @@ class RolesView(View):
     def get(self, request, id):
         role = models.Roles.objects.get(id=id)
         return render(request, "view_roles.html", {'role':role})
+    
+    def post(self, request, id):
+        models.Roles.objects.filter(id=id).delete()
+        return redirect("roles")
+    
+class EditRole(View):
+    def get(self, request, id):
+        role = models.Roles.objects.get(id=id)
+        form = forms.Editform(initial={"title" : role.title, "text": role.text})
+        return render(request, "edit_roles.html", {'form':form})
+    def post(self, request, id):
+        form = forms.Editform(request.POST)
+
+        if not form.is_valid():
+            return render(request, "edit_roles.html", {'form':form})
+        role = models.Roles.objects.get(id=id)
+        role.title = form.cleaned_data['title']
+        role.text = form.cleaned_data['text']
+        role.save()
+        return redirect('role', id=role.id)
 
 class CreateRoles(View):
     def get(self, request,):
         form = forms.Editform()
-        return render(request, "edit_announcements.html", {'form':form})
+        return render(request, "edit_roles.html", {'form':form})
     
     def post(self, request):
         form = forms.Editform(request.POST)
 
         if not form.is_valid():
             return render(request, "edit_roles.html", {'form':form})
+        
+        role = models.Roles(title=form.cleaned_data["title"], text=form.cleaned_data["text"])
+        role.save()
+
+        return redirect("role", id=role.id)
+            
