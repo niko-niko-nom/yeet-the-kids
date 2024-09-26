1382fe5f8ed0c9aa8e4d8a75aad5bb981bdb5315
niko-niko-nom <109971471+niko-niko-nom@users.noreply.github.com>
2024-09-26 11:09:25 +0200

    puwh

--- user_profiles/forms.py
+++ user_profiles/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class NewUserForm (forms.Form):
-    username = forms.Charfield(max_length=100, widget=forms.TextInput(attrs={
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
         'class': 'form-control',
         'placeholder': 'Enter a new name'
     }))
