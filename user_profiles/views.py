2b1bea2132735ffaf0102fd06b8c3f14bd1f5108
niko-niko-nom <109971471+niko-niko-nom@users.noreply.github.com>
2024-09-26 11:29:52 +0200

    test

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -45,6 +45,8 @@ def edit_profile(request, current_name):
     form = UserForm(initial={
         'pfp': user.pfp,
         'username': user.username,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
         'email': user.email,
         'age': user.birthday,
         'city': user.city,
